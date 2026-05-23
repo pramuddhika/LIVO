51d3bdbc7fd6f15120de25b0cbb8932f425e3e47
pramuddhika <pramuddhika.rasanga@gmail.com>
2026-05-23 14:41:44 +0530

    first commit

---
+++ livoBE/src/main/java/com/pramuddhika/livoBE/LivoBeApplication.java
@@ -0,0 +1,13 @@
+package com.pramuddhika.livoBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivoBeApplication.class, args);
+	}
+
+}
---
+++ livoBE/src/test/java/com/pramuddhika/livoBE/LivoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramuddhika.livoBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
